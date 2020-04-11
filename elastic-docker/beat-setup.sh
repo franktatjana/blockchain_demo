e35a76d2acf53eb2d56ae2d2aee27bd88a467d75
Tatjana Frank <frank.tatjana@yahoo.de>
2020-04-12 00:35:26 +0200

    update config

--- elastic-docker/beat-setup.sh
+++ elastic-docker/beat-setup.sh
@@ -2,28 +2,28 @@
 
 # If variable not present use default values
 : ${BLOCKCHAIN_CLUSTER_UUID:='BlockchainDemo:ZXVyb3BlLXdlc3QzLmdjcC5jbG91ZC5lcy5pbyQ4ZDVhMWY0ODc2MmI0OWNmYjZjODE4MzA3Y2I4NDAwOCQwZDM1MDcwMGY0YjM0YTY5ODhlNzRhMGQwN2QwYWM3Ng=='}
-: ${BLOCKCHAIN_CLUSTER_AUTH:='elastic:k8e1ObDYlCdreJj2YH29O9gM'}
+: ${BLOCKCHAIN_CLUSTER_PASS:='k8e1ObDYlCdreJj2YH29O9gM'}
 : ${ELASTIC_VERSION:='7.6.1'}
 
 export BLOCKCHAIN_CLUSTER_UUID BLOCKCHAIN_CLUSTER_AUTH ELASTIC_VERSION
 
 docker run \
-docker.elastic.co/beats/auditbeat:${ELASTIC_VERSION}\
-setup  -E cloud.id=${BLOCKCHAIN_CLUSTER_UUID} -E cloud.auth=elastic:${BLOCKCHAIN_CLUSTER_AUTH}
+docker.elastic.co/beats/auditbeat:${ELASTIC_VERSION} \
+setup  -E cloud.id=${BLOCKCHAIN_CLUSTER_UUID} -E cloud.auth=elastic:${BLOCKCHAIN_CLUSTER_PASS}
 
 
 docker run \
-docker.elastic.co/beats/heartbeat:${ELASTIC_VERSION}\
-setup  -E cloud.id=${BLOCKCHAIN_CLUSTER_UUID} -E cloud.auth=elastic:${BLOCKCHAIN_CLUSTER_AUTH}
+docker.elastic.co/beats/heartbeat:${ELASTIC_VERSION} \
+setup  -E cloud.id=${BLOCKCHAIN_CLUSTER_UUID} -E cloud.auth=elastic:${BLOCKCHAIN_CLUSTER_PASS}
 
 docker run \
-docker.elastic.co/beats/filebeat:${ELASTIC_VERSION}\
-setup  -E cloud.id=${BLOCKCHAIN_CLUSTER_UUID} -E cloud.auth=elastic:${BLOCKCHAIN_CLUSTER_AUTH}
+docker.elastic.co/beats/filebeat:${ELASTIC_VERSION} \
+setup  -E cloud.id=${BLOCKCHAIN_CLUSTER_UUID} -E cloud.auth=elastic:${BLOCKCHAIN_CLUSTER_PASS}
 
 docker run \
-docker.elastic.co/beats/metricbeat:${ELASTIC_VERSION}\
-setup  -E cloud.id=${BLOCKCHAIN_CLUSTER_UUID} -E cloud.auth=elastic:${BLOCKCHAIN_CLUSTER_AUTH}
+docker.elastic.co/beats/metricbeat:${ELASTIC_VERSION} \
+setup  -E cloud.id=${BLOCKCHAIN_CLUSTER_UUID} -E cloud.auth=elastic:${BLOCKCHAIN_CLUSTER_PASS}
 
 docker run \
-docker.elastic.co/beats/packetbeat:${ELASTIC_VERSION}\
-setup  -E cloud.id=${BLOCKCHAIN_CLUSTER_UUID} -E cloud.auth=elastic:${BLOCKCHAIN_CLUSTER_AUTH}
+docker.elastic.co/beats/packetbeat:${ELASTIC_VERSION} \
+setup  -E cloud.id=${BLOCKCHAIN_CLUSTER_UUID} -E cloud.auth=elastic:${BLOCKCHAIN_CLUSTER_PASS}
